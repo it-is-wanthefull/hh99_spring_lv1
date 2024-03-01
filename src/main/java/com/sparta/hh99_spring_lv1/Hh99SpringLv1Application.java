e2e2cc299f386b645cd080bcaabf16cfc81f8571
itiswanthefull <itiswanthefull@gmail.com>
2024-03-01 14:37:43 +0900

    일단 강의원본 Memo 그자체 조금씩 바꿔가보자!

---
+++ src/main/java/com/sparta/hh99_spring_lv1/Hh99SpringLv1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.hh99_spring_lv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh99SpringLv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hh99SpringLv1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/sparta/hh99_spring_lv1/dto/MemoRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.hh99_spring_lv1.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+}
